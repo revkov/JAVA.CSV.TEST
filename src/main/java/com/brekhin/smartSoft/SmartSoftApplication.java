68b3d232f1db112113a2c5f54b5bdffe1f4e17cd
Brekhin <brekhin@bk.ru>
2019-04-11 20:28:08 +0300

    initial commit

---
+++ src/main/java/com/brekhin/smartSoft/SmartSoftApplication.java
@@ -0,0 +1,13 @@
+package com.brekhin.smartSoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartSoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartSoftApplication.class, args);
+	}
+
+}
