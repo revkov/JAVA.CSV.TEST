c785a337d4166837c77e5d69dc4b4d328e22a25c
Brekhin <brekhin@bk.ru>
2019-04-13 22:10:06 +0300

    cache was enable because a operation with db(jpa) was very slow

--- src/main/java/com/brekhin/smartSoft/service/ActivityMonitoringService.java
+++ src/main/java/com/brekhin/smartSoft/service/ActivityMonitoringService.java
@@ -3,6 +3,7 @@ package com.brekhin.smartSoft.service;
 import com.brekhin.smartSoft.repository.projection.IFrequentlyUsedFormsTOProjection;
 import com.brekhin.smartSoft.to.out.ActivityInterruptedTO;
 import com.brekhin.smartSoft.to.out.FormsUsedInLastHourTO;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
